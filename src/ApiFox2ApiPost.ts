08c619c7e8fd36748c00d98c21804b38a160824c
xbw19975 <46275212+xbw19975@users.noreply.github.com>
2022-06-09 17:42:30 +0800

    fix
    raw

--- src/ApiFox2ApiPost.ts
+++ src/ApiFox2ApiPost.ts
@@ -179,6 +179,7 @@ class Apifox2Apipost {
           });
         }
       }else{
+        request.body.mode = 'json';
         request.body.raw=foxApi.requestBody.sampleValue || '';
       }
     }
